2e8d0486af30fbad64e39fc38aefd49788e26852
Fujiwara <r9.20important@icloud.com>
2021-06-03 19:12:18 +0900

    データをフロントに表示、リクエスト過多のバグあり

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -59,5 +59,9 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
+        //追加分
+        RateLimiter::for('seventy', function () {
+            return Limit::perMinute(70);
+        });
     }
 }
--- resources/ts/components/regist.tsx
+++ resources/ts/components/regist.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const regist:React.FC = () => {
+type Data = {
+    id: number;
+    month: number;
+    category: string;
+    detail: string;
+    money: number;
+    memo: string;
+    created_at: Date;
+    updated_at: Date;
+};
+
+const regist: React.VFC = () => {
+    const [datas, setDatas] = useState<Data[]>([]);
+    const getData = async () => {
+        const { data } = await axios.get<Data[]>("api/money");
+        setDatas(data);
+    };
+    useEffect(() => {
+        getData();
+    });
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <>
+            <div>Hello</div>
+            <ul>
+                {datas.map((data) => (
+                    <li key={data.id}>
+                        <div>
+                            <span>{data.category}</span>
+                            <button className="btn is-edit">編集</button>
+                            <button className="btn is-delete">削除</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
 
-export default regist
+export default regist;
